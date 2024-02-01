bc1cbfd60632cd61e5ab0ba3260d4b65f1e939d0
Dhruv Sharma <shrmadhruv11@gmail.com>
2024-02-01 23:07:19 +0530

    feat: linear search 2.0

--- Algorithms/Search/LinearSearch.js
+++ Algorithms/Search/LinearSearch.js
@@ -1,15 +1,12 @@
-function linearSearch(array, target) {
+const linearSearch1 = function linearSearch(array, target) {
   const length = array.length;
   for (let i = 0; i < length; i++) {
     if (array[i] === target) {
       return i;
     }
   }
-  return "Target not found";
-}
+  return -1;
+};
 
-console.log(linearSearch([1, 2, 3, 4, 5], 3));
-console.log(linearSearch([1, 2, 3, 4, 5], 6));
-console.log(linearSearch([1, 2, 3, 4, 5], 5));
-console.log(linearSearch([1, 2, 3, 4, 5], 1));
-console.log(linearSearch([1, 2, 3, 4, 5], 2));
+const result = linearSearch1([1, 2, 3, 4, 5], 4);
+console.log(result);
